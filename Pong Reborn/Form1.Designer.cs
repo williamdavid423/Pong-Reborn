e4d5667b4595d7c4205e0406844821972be68341
billy david <williamdavid423@hotmail.com>
2021-03-26 10:49:41 -0400

    Add project files.

---
+++ Pong Reborn/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Pong_Reborn
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.p1ScoreLabel = new System.Windows.Forms.Label();
+            this.p2ScoreLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
+            // 
+            // p1ScoreLabel
+            // 
+            this.p1ScoreLabel.AutoSize = true;
+            this.p1ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p1ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p1ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p1ScoreLabel.Location = new System.Drawing.Point(200, 10);
+            this.p1ScoreLabel.Name = "p1ScoreLabel";
+            this.p1ScoreLabel.Size = new System.Drawing.Size(18, 19);
+            this.p1ScoreLabel.TabIndex = 1;
+            this.p1ScoreLabel.Text = "0";
+            this.p1ScoreLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.p1ScoreLabel.Visible = false;
+            // 
+            // p2ScoreLabel
+            // 
+            this.p2ScoreLabel.AutoSize = true;
+            this.p2ScoreLabel.BackColor = System.Drawing.Color.Transparent;
+            this.p2ScoreLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.p2ScoreLabel.ForeColor = System.Drawing.Color.White;
+            this.p2ScoreLabel.Location = new System.Drawing.Point(400, 10);
+            this.p2ScoreLabel.Name = "p2ScoreLabel";
+            this.p2ScoreLabel.Size = new System.Drawing.Size(18, 19);
+            this.p2ScoreLabel.TabIndex = 2;
+            this.p2ScoreLabel.Text = "0";
+            this.p2ScoreLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.p2ScoreLabel.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("NewsGoth BdXCn BT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(50, 203);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 55);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Fast Pace";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(600, 400);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.p2ScoreLabel);
+            this.Controls.Add(this.p1ScoreLabel);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pong Reborn";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label p1ScoreLabel;
+        private System.Windows.Forms.Label p2ScoreLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Pong Reborn/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pong_Reborn
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+
+        int paddle1X = 10;
+        int paddle1Y = 100;
+        int player1Score = 0;
+
+        int paddle2X = 10;
+        int paddle2Y = 300;
+        int player2Score = 0;
+
+        int paddleWidth = 10;
+        int paddleHeight = 60;
+        int paddleSpeed = 4;
+
+        int ballX = 295;
+        int ballY = 195;
+        int ballXSpeed = 6;
+        int ballYSpeed = 6;
+        int ballWidth = 10;
+        int ballHeight = 10;
+
+
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+        bool aDown = false;
+        bool dDown = false;
+        bool leftArrowDown = false;
+        bool rightArrowDown = false;
+
+
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            switch (scene)
+            {
+                case 0:
+
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+            }
+        }
+    }
+}
